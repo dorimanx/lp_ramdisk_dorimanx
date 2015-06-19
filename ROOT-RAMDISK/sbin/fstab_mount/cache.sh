43826358c6bd98f52c1170c3cf1f70f0c1a70aba
Dorimanx <yuri@bynet.co.il>
2015-06-19 14:03:04 +0300

    Fix cache mounting as EXT4 when SELINUX is OFF. no seclabel mount.

--- ROOT-RAMDISK/sbin/fstab_mount/cache.sh
+++ ROOT-RAMDISK/sbin/fstab_mount/cache.sh
@@ -14,7 +14,7 @@ if [ "$CACHE" -eq "1" ]; then
 	/system/bin/fsck.f2fs -a /dev/block/platform/msm_sdcc.1/by-name/cache
 	$BB mount -t f2fs -o nosuid,nodev /dev/block/platform/msm_sdcc.1/by-name/cache /cache;
 else
-	$BB mount -t ext4 -o seclabel,nosuid,nodev,noauto_da_alloc,errors=continue /dev/block/platform/msm_sdcc.1/by-name/cache /cache;
+	$BB mount -t ext4 -o nosuid,nodev,noauto_da_alloc,errors=continue /dev/block/platform/msm_sdcc.1/by-name/cache /cache;
 fi;
 
 if [ ! -e /cache/lost+found ]; then
--- ROOT-RAMDISK/sbin/fstab_mount/system.sh
+++ ROOT-RAMDISK/sbin/fstab_mount/system.sh
@@ -7,5 +7,5 @@ SYSTEM=$($BB blkid /dev/block/platform/msm_sdcc.1/by-name/system | $BB grep "f2f
 if [ "${SYSTEM}" -eq "1" ]; then
 	$BB mount -t f2fs /dev/block/platform/msm_sdcc.1/by-name/system /system;
 else
-	$BB mount -t ext4 -o seclabel,noauto_da_alloc,errors=continue /dev/block/platform/msm_sdcc.1/by-name/system /system;
+	$BB mount -t ext4 -o noauto_da_alloc,errors=continue /dev/block/platform/msm_sdcc.1/by-name/system /system;
 fi;
