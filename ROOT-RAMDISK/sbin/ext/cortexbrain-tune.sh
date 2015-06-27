71f777db2c7d0458c7cc8ecb8357b9b54e8f150b
Dorimanx <yuri@bynet.co.il>
2015-06-27 22:56:57 +0300

    CORTEX: set mmap_min_addr to 32k to prevent null pointer dereference.
    Also set min_free_kbytes back to 4096 = 16MB to allow more RAM for apps.
    changing to to 64MB didnt help to prevent null pointer bugs.

--- ROOT-RAMDISK/sbin/ext/cortexbrain-tune.sh
+++ ROOT-RAMDISK/sbin/ext/cortexbrain-tune.sh
@@ -93,13 +93,6 @@ KERNEL_TWEAKS()
 	else
 		echo "kernel_tweaks disabled";
 	fi;
-	if [ "$cortexbrain_memory" == "on" ]; then
-		echo "32 32" > /proc/sys/vm/lowmem_reserve_ratio;
-
-		log -p i -t "$FILE_NAME" "*** MEMORY_TWEAKS ***: enabled";
-	else
-		echo "memory_tweaks disabled";
-	fi;
 }
 KERNEL_TWEAKS;
 
@@ -130,8 +123,10 @@ MEMORY_TWEAKS()
 		echo "1" > /proc/sys/vm/overcommit_memory; # default: 1
 		echo "50" > /proc/sys/vm/overcommit_ratio; # default: 50
 		echo "3" > /proc/sys/vm/page-cluster; # default: 3
-		echo "16384" > /proc/sys/vm/min_free_kbytes; #default: 2572
+		echo "4096" > /proc/sys/vm/min_free_kbytes; #default: 2572
 		# mem calc here in pages. so 16384 x 4 = 64MB reserved for fast access by kernel and VM
+		echo "32 32" > /proc/sys/vm/lowmem_reserve_ratio;
+		echo "65536" > /proc/sys/vm/mmap_min_addr; #default: 32768
 
 		log -p i -t "$FILE_NAME" "*** MEMORY_TWEAKS ***: enabled";
 	else
